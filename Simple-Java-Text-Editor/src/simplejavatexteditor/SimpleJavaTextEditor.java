612e2308013b49f1c4327fdc6b4706ed2be7916e
Daniel Gallego <64622798+Daniel-0ss@users.noreply.github.com>
2024-06-09 02:07:00 -0500

    Update SimpleJavaTextEditor.java

--- Simple-Java-Text-Editor/src/simplejavatexteditor/SimpleJavaTextEditor.java
+++ Simple-Java-Text-Editor/src/simplejavatexteditor/SimpleJavaTextEditor.java
@@ -18,9 +18,9 @@ import javax.swing.UnsupportedLookAndFeelException;
 public class SimpleJavaTextEditor extends JTextPane {
 
     private static final long serialVersionUID = 1L;
-    public final static String AUTHOR_EMAIL = "hi@ph7.me";
+    public final static String AUTHOR_EMAIL = "gallegodaniel62@gmail.com";
     public final static String NAME = "PHNotePad";
-    public final static String EDITOR_EMAIL = "contact@achinthagunasekara.com";
+    public final static String EDITOR_EMAIL = "gallegodaniel62@gmail.com";
     public final static double VERSION = 3.0;
 
     /**
