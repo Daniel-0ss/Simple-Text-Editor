c21657cc90193487588a602c7d30cbd808d35264
Daniel Gallego <64622798+Daniel-0ss@users.noreply.github.com>
2024-06-09 01:56:52 -0500

    Add files via upload

---
+++ Simple-Java-Text-Editor/src/simplejavatexteditor/FEdit.java
@@ -0,0 +1,22 @@
+/**
+ * @name        Simple Java NotePad
+ * @package     ph.notepad
+ * @file        UI.java
+ * @author      Daniel Gallego Granada
+ * @email       gallegodaniel62@gmail.com
+ * @link        https://github.com/Daniel-0ss
+ * @copyright   Copyright Daniel Gallego, All Rights Reserved.
+ */
+
+
+package simplejavatexteditor;
+
+import javax.swing.JTextArea;
+
+public class FEdit {
+
+    public static void clear(JTextArea textArea) {
+        textArea.setText("");
+    }
+
+}
---
+++ Simple-Java-Text-Editor/src/simplejavatexteditor/SimpleJavaTextEditor.java
@@ -0,0 +1,39 @@
+/**
+ * @name        Simple Java NotePad
+ * @package     ph.notepad
+ * @file        UI.java
+ * @author      Daniel Gallego Granada
+ * @email       gallegodaniel62@gmail.com
+ * @link        https://github.com/Daniel-0ss
+ * @copyright   Copyright Daniel Gallego, All Rights Reserved.
+ */
+
+
+package simplejavatexteditor;
+
+import javax.swing.JTextPane;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+public class SimpleJavaTextEditor extends JTextPane {
+
+    private static final long serialVersionUID = 1L;
+    public final static String AUTHOR_EMAIL = "hi@ph7.me";
+    public final static String NAME = "PHNotePad";
+    public final static String EDITOR_EMAIL = "contact@achinthagunasekara.com";
+    public final static double VERSION = 3.0;
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+    	try {
+    		UIManager.setLookAndFeel ("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+    	} catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) {
+    		e.printStackTrace();
+    	}
+    	
+        new UI().setVisible(true);
+    }
+
+}
